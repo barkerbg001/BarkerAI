80d0cf668c2c06972fd5f0f18c8b3e406bb59afd
Bradley Barker <Bradley.Barker@thoughtware.co.za>
2022-11-10 16:50:55 +0200

    Started a Collectionview

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,7 +14,15 @@ public partial class MainPage : ContentPage
         var serv = new CraiyonService(1);
         string mainDir = FileSystem.Current.AppDataDirectory;
         string filePath = Path.Combine(mainDir, edtPrompt.Text);
+
+        var docs = new List<string>();
+        for (int i = 0 ; i < 9 ; i++)
+        {
+            docs.Add(Path.Combine(filePath, i.ToString() + ".jpg"));
+        }
         await serv.DownloadGalleryAsync(edtPrompt.Text, filePath);
+        cvImages.ItemsSource= docs;
+        cvImages.IsVisible= true;
     }
 }
 
